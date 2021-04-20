9dac2a97aed6b8b981c6e6c269805fb1f6e7b0d8
Dima Khristich <dkhristich@ebay.com>
2021-04-20 22:46:33 +0300

    random lists

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import requests
 import logging
+import random
 from datetime import datetime, timedelta
 from aiogram import Bot, Dispatcher, executor, types
 from math import ceil
@@ -54,6 +55,26 @@ def get_prisoners():
     return prisoners
 
 
+def shuffle_prisoners():
+    cache_prisoners()
+    current_prisoners = cached_prisoners['prisoners']
+    current_friends_count = current_prisoners[0]['fields']['friendsCount']
+    temp_prisoners = list()
+    shuufled_prisoners = list()
+    for prisoner in current_prisoners:
+        prisoner_friends_count = prisoner['fields']['friendsCount']
+        if prisoner_friends_count == current_friends_count:
+            temp_prisoners.append(prisoner)
+        else:
+            random.shuffle(temp_prisoners)
+            shuufled_prisoners.extend(temp_prisoners)
+            temp_prisoners = [prisoner]
+            current_friends_count = prisoner_friends_count
+    random.shuffle(temp_prisoners)
+    shuufled_prisoners.extend(temp_prisoners)
+    cached_prisoners['prisoners'] = shuufled_prisoners
+
+
 def cache_prisoners():
     now = datetime.now()
     if 'time' not in cached_prisoners or cached_prisoners['time'] < now - timedelta(minutes=1):
@@ -76,7 +97,7 @@ def format_prisoner(prisoner):
 
 
 def get_prisoners_by_count(n=10):
-    cache_prisoners()
+    shuffle_prisoners()
     filtered_prisons = list()
     filtered_prisons.extend(x for x in cached_prisoners['prisoners'][:n])
     for i in range(ceil(len(filtered_prisons) / bulk)):
